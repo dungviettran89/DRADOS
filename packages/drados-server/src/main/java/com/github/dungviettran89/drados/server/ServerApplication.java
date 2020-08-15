907d01b5597dcea3497d759a046c4de446518ba0
dungviettran89 <dungviettran89@gmail.com>
2020-08-15 16:27:21 +0800

    Move from NestJS to java for faster developement

---
+++ packages/drados-server/src/main/java/com/github/dungviettran89/drados/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.dungviettran89.drados.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ packages/drados-server/src/test/java/com/github/dungviettran89/drados/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dungviettran89.drados.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
